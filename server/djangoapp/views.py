e1740194a67567f0153113445c464409066d9aab
CongWang5h <congwang.5h@gmail.com>
2023-11-14 11:12:33 -0500

    add views

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
+
 # from .models import related models
 # from .restapis import related methods
 from django.contrib.auth import login, logout, authenticate
@@ -18,12 +19,18 @@ logger = logging.getLogger(__name__)
 
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, "djangoapp/about.html", context)
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, "djangoapp/contact.html", context)
+
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
@@ -37,11 +44,12 @@ logger = logging.getLogger(__name__)
 # def registration_request(request):
 # ...
 
+
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        return render(request, "djangoapp/index.html", context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
@@ -51,4 +59,3 @@ def get_dealerships(request):
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
-
