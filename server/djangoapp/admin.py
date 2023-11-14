ae71afad084ac0ca8f8ed48ab678a49c78202a4c
CongWang5h <congwang.5h@gmail.com>
2023-11-14 17:15:50 -0500

    add admin

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
-# from .models import related models
+from .models import related models
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+	model = CarModel
+	extra = 3
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+	list_display = ("name", "type", "year")
+	inlines = [CarModelInline]
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+	list_display = ("name", "description")
+	inlines = [CarModelInline]
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
