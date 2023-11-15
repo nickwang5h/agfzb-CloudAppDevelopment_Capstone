5ba75723c7bc49b61d520844fdcbddfb208ca94a
CongWang5h <congwang.5h@gmail.com>
2023-11-14 19:05:31 -0500

    ajidfjo

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -3,7 +3,7 @@ import json
 
 # import related models here
 from requests.auth import HTTPBasicAuth
-from models import CarDealer, DealerReview
+from .models import CarDealer, DealerReview
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_watson.natural_language_understanding_v1 import (
